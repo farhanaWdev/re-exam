3a9efb32c24eb34c74c7a508ff1361dcdb2a1447
farhanaWdev <wfarhanaislam@gmail.com>
2026-09-21 23:18:45 +0600

    update controller

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-// require('node:dns').setServers(['1.1.1.1','8.8.8.8'])
-// require('dotenv').config()
-// const express = require('express')
-// const mongoose = require('mongoose')
+require('node:dns').setServers(['1.1.1.1','8.8.8.8'])
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
 
-// const authRoute = require('./routes/authRoute')
-// const dbConnection = require('./config/dbConnection');
-// const app =express()
-// dbConnection()
+const authRoute = require('./routes/authRoute')
+const dbConnection = require('./config/dbConnection');
+const app =express()
+dbConnection()
 
-// app.use(express.json())
+app.use(express.json())
 
 
-// app.use('/api/v1/auth', authRoute)
+app.use('/api/v1/auth', authRoute)
 
-// app.listen(5000,()=>{
-//     console.log('server is running on port')
-// })
+app.listen(5000,()=>{
+    console.log('server is running on port')
+})
